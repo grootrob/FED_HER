b3ebc8f07c9bbbe1e998db7e9ed1e83ee28b07fa
Rob de Groot <groot_rob@live.nl>
2019-11-15 12:24:09 +0100

    nove15

--- Eindopdracht/ns_her/script.js
+++ Eindopdracht/ns_her/script.js
@@ -1,3 +1,7 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
 const filter = document.querySelector('#filterShow');
 const button = document.querySelector('.filterenShow');
 const toon = document.querySelector('#toon');
@@ -10,12 +14,17 @@ button.addEventListener('click', function () {
 
 });
 
-//const likeKnop = document.querySelector('#filterShow');
+
 //
+//var zoekveld = document.querySelector('.searchbar');
+//var zoekbutton = document.querySelector('.zoekknoppie');
+//var zoeklabel = document.querySelector('figcaption');
 //
-//button.addEventListener('click', function () {
-//    filter.classList.toggle('show');
-//    toon.classList.toggle('show');
-//    sluit.classList.toggle('show');
-//
-//});
+//function show() {
+//    zoekveld.classList.toggle('show-search');
+//    if (zoekveld.classList.contains('show-search')) {
+//        showClose();
+//    } else {
+//        showSearch();
+//    }
+//}
